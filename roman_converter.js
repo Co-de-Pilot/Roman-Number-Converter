5c7dda729cc16e034f0b3ef266810b97d7333a25
CO_DE_PILOT <piratosnogi@gmail.com>
2024-05-11 14:55:29 +0200

    .js file refactor

--- roman_converter.js
+++ roman_converter.js
@@ -1,57 +1,54 @@
-const digits = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-const romanDigits = [
-  "M",
-  "CM",
-  "D",
-  "CD",
-  "C",
-  "XC",
-  "L",
-  "XL",
-  "X",
-  "IX",
-  "V",
-  "IV",
-  "I",
-];
+/* ----------------------------------------------- */
+/* A római és arab számjegy konstansok             */
+/* ----------------------------------------------- */
+const convertRoman = {
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1,
+};
+const romanDigits = Object.keys(convertRoman);
+const arabicDigits = Object.values(convertRoman);
 
-let intToRoman = function (number) {
+/* ----------------------------------------------- */
+/* Arab számról rómaira konvertálás                */
+/* ----------------------------------------------- */
+let arabicToRoman = function (inputNumber) {
   let roman = "";
-  digits.map((digit, index) => {
-    while (number >= digit) {
+  arabicDigits.map((digit, index) => {
+    while (inputNumber >= digit) {
       roman += romanDigits[index];
-      number -= digit;
+      inputNumber -= digit;
     }
   });
   return roman;
 };
 
-let romanToInt = function (s) {
-  let convertRoman = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000,
-  };
-  let characterArray = s.split("");
+/* ----------------------------------------------- */
+/* Római számról arabra konvertálás                */
+/* ----------------------------------------------- */
+let romanToArabic = function (InputString) {
+  let characterArray = InputString.split("");
   let numberArray = characterArray.map(
     (character) => (character = convertRoman[character])
   );
-  let valueArray = numberArray.map((character, index) =>
-    negativSetter(index, numberArray)
-  );
-  return valueArray.reduce((acc, number) => acc + number);
+  return numberArray.reduce((acc, number, index) => {
+    if (numberArray[index] < numberArray[index + 1]) {
+      return acc - number;
+    } else {
+      return acc + number;
+    }
+  });
 };
-
-function negativSetter(i, array) {
-  if (array[i] < array[i + 1]) {
-    array[i] = -Math.abs(array[i]);
-  }
-  return array[i];
-}
 
 /* ----------------------------------------------- */
 /* Az INPUT és BUTTON konstansok meghatározása     */
@@ -91,9 +88,9 @@ romanField.addEventListener("keyup", () => {
 /* ----------------------------------------------- */
 convertButton.addEventListener("click", () => {
   if (arabicField.reportValidity() && arabicField.value != "") {
-    resultField.value = intToRoman(Number.parseInt(arabicField.value));
+    resultField.value = arabicToRoman(Number.parseInt(arabicField.value));
   } else if (romanField.reportValidity() && romanField.value != "") {
-    resultField.value = romanToInt(romanField.value);
+    resultField.value = romanToArabic(romanField.value);
   } else {
     resultField.value = "Please enter a valid input!";
   }
